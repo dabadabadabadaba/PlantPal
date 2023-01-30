c0a57213a41ae2ad46992cd99cbdfcb0677b451d
dabadabadabadaba <113569392+dabadabadabadaba@users.noreply.github.com>
2023-01-30 11:36:13 -0800

    healthbard increases instead of decreases! still need to trigger with mouse event instead of keyboard

--- Assets/Plant.cs
+++ Assets/Plant.cs
@@ -3,33 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// public class Plant : MonoBehaviour
+// {
+
+// 	public int maxHealth = 100;
+// 	public int currentHealth;
+
+// 	public HealthBar healthBar;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+// 		currentHealth = maxHealth;
+// 		healthBar.SetMaxHealth(maxHealth);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+// 		if (Input.GetKeyDown(KeyCode.Space))
+// 		{
+// 			TakeDamage(20);
+// 		}
+//     }
+
+// 	void TakeDamage(int damage)
+// 	{
+// 		currentHealth -= damage;
+// 		healthBar.SetHealth(currentHealth);
+// 	}
+// }
+
 public class Plant : MonoBehaviour
 {
 
-	public int maxHealth = 100;
+
 	public int currentHealth;
 
 	public HealthBar healthBar;
 
-    // Start is called before the first frame update
     void Start()
     {
-		currentHealth = maxHealth;
-		healthBar.SetMaxHealth(maxHealth);
+		currentHealth = 0;
+		healthBar.SetHealth(currentHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			TakeDamage(20);
+			AddHealth(5);
 		}
     }
 
-	void TakeDamage(int damage)
+	void AddHealth(int health)
 	{
-		currentHealth -= damage;
+		currentHealth += health;
 		healthBar.SetHealth(currentHealth);
 	}
 }
