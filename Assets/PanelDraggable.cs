daff68d3bab157480cfac4c3f2cb22bc4415a9a2
dabadabadabadaba <113569392+dabadabadabadaba@users.noreply.github.com>
2023-02-07 21:35:54 -0800

    cleaned up files, panel works as expected with hat but not the other items

--- Assets/PanelDraggable.cs
+++ Assets/PanelDraggable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComplexDraggable : MonoBehaviour
+public class PanelDraggable : MonoBehaviour
 {
     public GameObject Panel;
     public GameObject Plant;
@@ -22,7 +22,6 @@ public class ComplexDraggable : MonoBehaviour
             // //will send the object back to starting position
             // transform.position = startPosition;
             
-            // RaycastHit2D[] hits;
             Collider2D[] hits;
             hits = Physics2D.OverlapPointAll(transform.position);
             Debug.Log(hits.Length);
@@ -30,34 +29,27 @@ public class ComplexDraggable : MonoBehaviour
 
             foreach(var obj in hits)
             {
-                try
+                Collider2D foundComponent = obj.GetComponent<Collider2D>();
+                Debug.Log(foundComponent.tag);
+                Debug.Log(foundComponent.name);
+
+                if (foundComponent.tag == "Plant")
                 {
-                    Collider2D foundComponent = obj.GetComponent<Collider2D>();
-                    Debug.Log(foundComponent.tag);
-                    Debug.Log(foundComponent.name);
-                    // foundComponent.TakeOwnership(this);
-                    // return;
+                    Debug.Log("Setting parent to plant");
+                    transform.SetParent(Plant.transform);
+                    // set item position to plant position
                 }
 
-                catch
+                if (foundComponent.tag == "Panel")
                 {
-                    //no component found
+                    Debug.Log("Setting parent to panel");
+                    transform.SetParent(Panel.transform);
                 }
 
             }
-            Debug.Log("Setting parent to plant");
-            isDragging = false;
-            transform.SetParent(Plant.transform);
-        }
 
-        // private void OnTriggerEnter2D(Collider2D other) 
-        // {
-        //     if (other.tag == "Panel") 
-        //     {
-        //         Debug.Log("Add item back to menu");
-        //         transform.SetParent(Panel.transform);
-        //     }
-	    // }
+            isDragging = false;
+        }
 
         void Update() 
         {
